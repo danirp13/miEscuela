1be9d1155924bc6a4e1d4b1ee0dcf9d233a8cfa1
Paula <paulamunoz@umariana.edu.co>
2017-04-04 17:31:21 -0500

    se registro estudiante y materia en el adm

--- academica/admin.py
+++ academica/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from academica.models import Estudiante, Materia
 # Register your models here.
+
+admin.site.register(Estudiante)
+admin.site.register(Materia)
--- academica/models.py
+++ academica/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Materia(models.Model):
+	"""docstring for Materia"""
+	nombre=models.CharField(max_length=50,blank=True)
+
+	def __str__(self):
+		return self.nombre
+
 class  Estudiante(models.Model):
 	nombres=models.CharField(max_length=50,blank=True)
 	apellido=models.CharField(max_length=50,blank=True)
 	direccion=models.CharField(max_length=50,blank=True)
 	email=models.EmailField(max_length=50)
 	#photo=models.ImageField(upload_to='photo/')
-class Materia(models.Model):
-	"""docstring for Materia"""
-	nombre=models.CharField(max_length=50,blank=True)
+	materia=models.ForeignKey(Materia)
 
+	def __str__(self):
+		return self.nombres+" "+self.apellido
