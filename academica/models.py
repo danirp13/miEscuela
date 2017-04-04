8a1f3f9c728407de17ad29bfa4587c7cd3fd0be8
Paula <paulamunoz@umariana.edu.co>
2017-04-04 17:18:04 -0500

    Se crearon modelo estudiante y materia

--- academica/models.py
+++ academica/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class  Estudiante(models.Model):
+	nombres=models.CharField(max_length=50,blank=True)
+	apellido=models.CharField(max_length=50,blank=True)
+	direccion=models.CharField(max_length=50,blank=True)
+	email=models.EmailField(max_length=50)
+	#photo=models.ImageField(upload_to='photo/')
+class Materia(models.Model):
+	"""docstring for Materia"""
+	nombre=models.CharField(max_length=50,blank=True)
+
